cc72945b2d355f0c34893f73b90acb0c782f0e70
= <=>
2025-03-22 16:42:09 -0300

    send SMS message from twilio with the news of the stock the user chose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from twilio.rest import Client
 
 load_dotenv('.env')
 
@@ -13,6 +14,10 @@ TWILIO_ENDPOINT = "https://www.twilio.com"
 
 STOCK_API_KEY = os.getenv('STOCK_API_KEY')
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+TWILIO_SID = os.getenv('TWILIO_SID')
+TWILIO_TOKEN = os.getenv('TWILIO_TOKEN')
+TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
+MY_NUMBER = os.getenv("MY_NUMBER")
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -25,19 +30,21 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-print(yesterday_closing_price)
 
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+up_down = None
+if difference > 0:
+    up_down = "⬆️"
+else:
+    up_down = "⬇️"
 
-diff_percent = (difference / float(yesterday_closing_price)) * 100
-print(diff_percent)
 
-if diff_percent > 5:
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+if abs(diff_percent) > 5:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -47,6 +54,16 @@ if diff_percent > 5:
     articles = news_response.json()["articles"]
     three_articles = articles[:3]
 
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            from_=TWILIO_NUMBER,
+            body=article,
+            to=MY_NUMBER
+        )
 
 #Optional: Format the SMS message like this: 
 # """
