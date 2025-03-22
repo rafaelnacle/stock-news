7120e9bb47d9b2674121183bcc955300441618fc
= <=>
2025-03-22 16:26:24 -0300

    get the first three articles that talks about the company name

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 TWILIO_ENDPOINT = "https://www.twilio.com"
 
 STOCK_API_KEY = os.getenv('STOCK_API_KEY')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -37,7 +38,15 @@ diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
 if diff_percent > 5:
-    print("Get News")
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+
 
 #Optional: Format the SMS message like this: 
 # """
